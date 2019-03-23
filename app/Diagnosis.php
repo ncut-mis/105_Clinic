c9723d655e2fb4d4302ead7c9c3104b9c51f18ae
江珮妤 <Luxgen077@gmail.com>
2019-03-24 01:59:04 +0800

    建立Diagnosis的關聯

--- app/Diagnosis.php
+++ app/Diagnosis.php
@@ -3,10 +3,39 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Patient as PatientEloquent;
+use \App\Section as SectionEloquent;
+use \App\Doctor as DoctorEloquent;
+use \App\Prescription as PrescriptionEloquent;
+use \App\Clinic as ClinicEloquent;
 
 class Diagnosis extends Model
 {
     protected $fillable = [
         'member_id', 'doctor_id', 'symptom',
     ];
+    public function patient()
+    {
+        return $this->belongsTo(PatientEloquent::class);
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(SectionEloquent::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(DoctorEloquent::class);
+    }
+
+    public function prescription()
+    {
+        return $this->hasMany(PrescriptionEloquent::class);
+    }
+
+    public function clinic()
+    {
+        return $this->belongsTo(ClinicEloquent::class);
+    }
 }
