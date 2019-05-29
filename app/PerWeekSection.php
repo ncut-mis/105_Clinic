a022c803e46f4248d105fc30f5d511ed01a1c077
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2019-05-30 00:15:48 +0800

    建立每週看診時段model關聯

--- app/PerWeekSection.php
+++ app/PerWeekSection.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PerWeekSection extends Model
 {
-    //
+    protected $fillable = [
+        'doctor_id', 'weekday', 'start_time','end_time','from','suspense_from','suspense_to'
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+
 }
