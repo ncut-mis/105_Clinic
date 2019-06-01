b7f7487011ea8d64519135718f3cad5dba0ce91b
江珮妤 <Luxgen077@gmail.com>
2019-06-02 03:22:50 +0800

    firebase 提醒號碼通知

--- app/Http/Controllers/DiagnosisController.php
+++ app/Http/Controllers/DiagnosisController.php
@@ -11,6 +11,12 @@ use App\Prescription;
 use App\Register;
 use App\Staff;
 use Illuminate\Http\Request;
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use FCM;
+use  Auth;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class DiagnosisController extends Controller
 {
@@ -37,6 +43,7 @@ class DiagnosisController extends Controller
         $time=date("H:i:s");
         $current_section=$doctor->sections()->where('date',$date)->where('start','<',$time)->where('end','>',$time)->get()->first();//目前的看診時段
         $one=$current_section->registers()->where('status',1)->get()->first();
+
         if($one!==null)
         {
             session(['one' => $one]);
@@ -45,12 +52,43 @@ class DiagnosisController extends Controller
             $one_no->save();
             return redirect()->route('firebase.late',$patient);
         }
-
         $current=session('next');
         $current->status=1;  //已呼叫
         $current_section->update(['current_no' =>$current->number]);
         $current->save();
         session(['current' => $current]);
+
+        $registers=$current_section->registers()->get();
+        foreach ($registers as $register)
+        {
+            if($register->reminding_no!==0)
+            {
+              $if_equal_current_no = $register->number - $register->reminding_no;
+             if($current_section->current_no===$if_equal_current_no)
+              {
+               $optionBuilder = new OptionsBuilder();
+               $optionBuilder->setTimeToLive(60*20);
+               $notificationBuilder = new PayloadNotificationBuilder(Auth::user()->clinic->name.'通知');
+               $notificationBuilder->setBody('目前看診號碼為'.$current_section->current_no.'號,還有'.$register->reminding_no.'位就輪到您囉')->setSound('default');
+               $dataBuilder = new PayloadDataBuilder();
+               $dataBuilder->addData(['a_data' => 'my_data']);
+               $option = $optionBuilder->build();
+               $notification = $notificationBuilder->build();
+               $data = $dataBuilder->build();
+               $token = "fXBRQnqdcVo:APA91bEVvrBRXL7VyCiIikWeQFPvk7VvH4KUmFuh1pZFItkRaREdWkHOYhp6PBBsU5NxV9CtXCGbWSn631kNAvz6few6cEsrU-0qkvkgPSz_Vg5g-SgAS5eXGiC-QrNBr5_uZTjar5Qm";
+               $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+               $downstreamResponse->numberSuccess();
+               $downstreamResponse->numberFailure();
+               //return Array - you must remove all this tokens in your database
+               $downstreamResponse->tokensToDelete();
+               //return Array (key : oldToken, value : new token - you must change the token in your database )
+               $downstreamResponse->tokensToModify();
+               //return Array - you should try to resend the message to the tokens in the array
+               $downstreamResponse->tokensToRetry();
+               // return Array (key:token, value:errror) - in production you should remove from your database the tokens
+              }
+            }
+        }
 
         $waiting_list=$current_section->registers()->where('status',0)->orderBy('number', 'ASC')->get();
         $next=$waiting_list->first(); //下一個看診者
