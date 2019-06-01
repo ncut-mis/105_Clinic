c614cf0a49d84723ea76c10afb3fd55886dc7993
江珮妤 <Luxgen077@gmail.com>
2019-06-02 03:21:28 +0800

    修改過號通知的路由名稱

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::patch('/clinic/information'  , ['as' => 'clinic.information.update', 'use
 
 
 //firebase測試
-Route::get('/fire/{patient}'  , ['as' => 'firebase.late', 'uses' => 'NoticeController@late']);
+Route::get('/firebase/late/{patient}'  , ['as' => 'firebase.late', 'uses' => 'NoticeController@late']);
 
 
 //掛號相關路由..................................................................................
