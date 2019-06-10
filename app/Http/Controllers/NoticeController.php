3e960012912baff0a19f74412011b9eaad4ac054
江珮妤 <Luxgen077@gmail.com>
2019-06-10 12:07:31 +0800

    通知設定

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -16,16 +16,17 @@ class NoticeController extends Controller
 {
     public function late(Patient $patient)
     {
+
         $optionBuilder = new OptionsBuilder();
         $optionBuilder->setTimeToLive(60*20);
-        $notificationBuilder = new PayloadNotificationBuilder('診所通知');
+        $notificationBuilder = new PayloadNotificationBuilder(Auth::user()->clinic->name.'通知');
         $notificationBuilder->setBody('您已過號，若要看診煩請回到診所重新掛號!')->setSound('default');
         $dataBuilder = new PayloadDataBuilder();
         $dataBuilder->addData(['a_data' => 'my_data']);
         $option = $optionBuilder->build();
         $notification = $notificationBuilder->build();
         $data = $dataBuilder->build();
-        $token = "fXBRQnqdcVo:APA91bEVvrBRXL7VyCiIikWeQFPvk7VvH4KUmFuh1pZFItkRaREdWkHOYhp6PBBsU5NxV9CtXCGbWSn631kNAvz6few6cEsrU-0qkvkgPSz_Vg5g-SgAS5eXGiC-QrNBr5_uZTjar5Qm";
+        $token = "dmoIXyCO97w:APA91bEUJTZuHldyxknmky6bkYNcmUM8c0rx5kwm-4XbeMCk1wIIuQ2hEYUzG7XJVxnf5wRlHEPoyGF16J1YlYeFO4_MtLtREDmuurJK9ZjMbY6Zg6DB0HhiWN9o-j5LVXHanfOHlwf4";
         $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
         $downstreamResponse->numberSuccess();
         $downstreamResponse->numberFailure();
@@ -37,31 +38,32 @@ class NoticeController extends Controller
         $downstreamResponse->tokensToRetry();
 // return Array (key:token, value:error) - in production you should remove from your database the tokens
 //        return redirect()->route('doctor.diagnosis',$patient);
+        return redirect()->route('doctor.diagnosis',$patient);
     }
 
     public function number(Section $current_section,Register $register,Patient $patient)
     {
-//        $optionBuilder = new OptionsBuilder();
-//        $optionBuilder->setTimeToLive(60*20);
-//        $notificationBuilder = new PayloadNotificationBuilder(Auth::user()->clinic->name.'通知');
-//        $notificationBuilder->setBody('目前看診號碼為'.$current_section->current_no.'號,還有'.$register->reminding_no.'位就輪到您囉')->setSound('default');
-//        $dataBuilder = new PayloadDataBuilder();
-//        $dataBuilder->addData(['a_data' => 'my_data']);
-//        $option = $optionBuilder->build();
-//        $notification = $notificationBuilder->build();
-//        $data = $dataBuilder->build();
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60*20);
+        $notificationBuilder = new PayloadNotificationBuilder(Auth::user()->clinic->name.'通知');
+        $notificationBuilder->setBody('目前看診號碼為'.$current_section->current_no.'號,還有'.$register->reminding_no.'位就輪到您囉')->setSound('default');
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['a_data' => 'my_data']);
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
 //        $token = "fYaSghNWxMY:APA91bEdW_uSrWmQZvVjlYHQ0SYumSjmO0-18hyM2PE9VFIHbeDSTNNVtmp-zJ-uRJ20Y1YgMPreVbabExE1Gnfmybw72d0bCUBMZfMNVLW4CqcaCxawkxabYAYCKvsGsUmZVOIp5oaO";
-//        $token = "fXBRQnqdcVo:APA91bEVvrBRXL7VyCiIikWeQFPvk7VvH4KUmFuh1pZFItkRaREdWkHOYhp6PBBsU5NxV9CtXCGbWSn631kNAvz6few6cEsrU-0qkvkgPSz_Vg5g-SgAS5eXGiC-QrNBr5_uZTjar5Qm";
-//        $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
-//        $downstreamResponse->numberSuccess();
-//        $downstreamResponse->numberFailure();
-////return Array - you must remove all this tokens in your database
-//        $downstreamResponse->tokensToDelete();
-////return Array (key : oldToken, value : new token - you must change the token in your database )
-//        $downstreamResponse->tokensToModify();
-////return Array - you should try to resend the message to the tokens in the array
-//        $downstreamResponse->tokensToRetry();
-//// return Array (key:token, value:error) - in production you should remove from your database the tokens
+        $token = "fXBRQnqdcVo:APA91bEVvrBRXL7VyCiIikWeQFPvk7VvH4KUmFuh1pZFItkRaREdWkHOYhp6PBBsU5NxV9CtXCGbWSn631kNAvz6few6cEsrU-0qkvkgPSz_Vg5g-SgAS5eXGiC-QrNBr5_uZTjar5Qm";
+        $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+        $downstreamResponse->numberSuccess();
+        $downstreamResponse->numberFailure();
+//return Array - you must remove all this tokens in your database
+        $downstreamResponse->tokensToDelete();
+//return Array (key : oldToken, value : new token - you must change the token in your database )
+        $downstreamResponse->tokensToModify();
+//return Array - you should try to resend the message to the tokens in the array
+        $downstreamResponse->tokensToRetry();
+// return Array (key:token, value:error) - in production you should remove from your database the tokens
 //        return redirect()->route('doctor.diagnosis',$patient);
     }
 
@@ -80,7 +82,7 @@ class NoticeController extends Controller
             foreach ($registers as $register)
             {
 //                echo $register->reminding_time;
-            if($register->reminding_time!=="00:00:00")
+            if($register->reminding_time!==null||$register->reminding_time!=="00:00:00")
             {
 //                echo $current->start;
 //                echo $register->reminding_time;
@@ -88,7 +90,7 @@ class NoticeController extends Controller
 //                $if_equal_time = ($current->start - $register->reminding_time)/60;
 //                dd(date("H:i:s",$register->reminding_time))  ;
                   $hour=date("H");//目前的小時
-                if($hour=== "$if_equal_time")
+                if($hour=== "$if_equal_time")//24h
                 {
                     $optionBuilder = new OptionsBuilder();
                     $optionBuilder->setTimeToLive(60*20);
@@ -99,7 +101,7 @@ class NoticeController extends Controller
                     $option = $optionBuilder->build();
                     $notification = $notificationBuilder->build();
                     $data = $dataBuilder->build();
-                    $token = "dqp9esKsFTA:APA91bG7IEtk_Cw7Ivi-RqqSmfAZLTt4mZ8PSwJ6HxKmnBcsE3IaAUfdAjsy00yfwVkR_5dQhG2XdmPJqnG1WaxUS_Xj1HWJjrtQXIO1ddLAEKOLHj56BFgKXdiQ1BpPrprUhaeTnS5t";
+                    $token = "fXBRQnqdcVo:APA91bEVvrBRXL7VyCiIikWeQFPvk7VvH4KUmFuh1pZFItkRaREdWkHOYhp6PBBsU5NxV9CtXCGbWSn631kNAvz6few6cEsrU-0qkvkgPSz_Vg5g-SgAS5eXGiC-QrNBr5_uZTjar5Qm";
                     $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
                     $downstreamResponse->numberSuccess();
                     $downstreamResponse->numberFailure();
